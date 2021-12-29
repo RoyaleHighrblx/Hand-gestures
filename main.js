2e7ba0b48d411f41328c531050ab0beb89a1cc70
RoyaleHighrblx <96347364+RoyaleHighrblx@users.noreply.github.com>
2021-12-28 21:00:39 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -28,3 +28,46 @@ function Speak(){
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + data_speak_2);
     synth.speak(utterThis);
 }
+
+function Check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        document.getElementById("result_gesture_name_2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        Speak()
+        
+        if(results[0].label == "peace")
+        {
+        document.getElementById("update_gesture").innerHTML = "&#9996; - Well that was easy";
+        }
+        if(results[0].label == "thumbs up")
+        {
+        document.getElementById("update_gesture").innerHTML = "&#128077; - That's good";
+        }
+        if(results[0].label == "thumbs down")
+        {
+        document.getElementById("update_gesture").innerHTML = "&#128078; - That's bad";
+        }
+        if(results[0].label == "peace")
+        {
+        document.getElementById("update_gesture_2").innerHTML = "&#9996; - Well that was easy";
+        }
+        if(results[0].label == "thumbs up")
+        {
+        document.getElementById("update_gesture_2").innerHTML = "&#128077; - That's good";
+        }
+        if(results[0].label == "thumbs down")
+        {
+        document.getElementById("update_gesture_2").innerHTML = "&#128078; - That's bad";
+        }
+    }
+}
